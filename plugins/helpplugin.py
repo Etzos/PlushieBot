65de063c17e119c6a5a0e5793651522ea4173f25
Kitsune30 <ckey1793@gmail.com>
2015-02-19 03:37:10 -0600

    Corrects comprehension issues in Help plugin. Adds the Help plugin to the list of plugins to start on PlushieBot start

--- plugins/helpplugin.py
+++ plugins/helpplugin.py
@@ -3,7 +3,7 @@ from .plugin import *
 class HelpPlugin(PlushiePlugin):
     name = "PlushieBot Help Plugin"
     description = "Access documentation for PlushieBot commands"
-    authors = ["WhiteKitsune"]
+    authors = ["Garth"]
     
     def __init__(self):
         self.documentation = None
@@ -34,13 +34,20 @@ class HelpPlugin(PlushiePlugin):
             self.follows = args[-2:-1]
         
         try:
-            self.documentation = [(x.doc, x.extra) for x in ctx.parent.commands[self.cmd]._doc if x.follows == self.follows and (x.cmd == self.subcmd or self.subcmd in x.alias)]
+            self.documentation = [(x['doc'], x['extra']) for x in ctx.parent.commands[self.cmd]._doc 
+                                    if x['follows'] == self.follows and (x['cmd'] == self.subcmd 
+                                    or self.subcmd in x['alias'])][0]
         except:
-            ctx.msg("[Plugin:Help] Error: Could not run the comprehension to find documentations. Contact Garth or WhiteKitsune.", msg.replyTo)
+            ctx.msg("[Plugin:Help] Error: Could not run the comprehension to find documentations.", msg.replyTo)
             return
         try:
-            self.followingcmds = [x.cmd for x in ctx.parent.commands[self.cmd]._doc if x.follows == self.follows]
+            should_follow = None if not self.subcmd else args[-1:]
+            self.followingcmds = [x['cmd'] for x in ctx.parent.commands[self.cmd]._doc if x['follows'] == should_follow and x['cmd']]
         except:
-            ctx.msg("[Plugin:Help] Error: Could not run the comprehension for following subcommands. Contact Garth or WhiteKitsune.", msg.replyTo)
+            ctx.msg("[Plugin:Help] Error: Could not run the comprehension for following subcommands.", msg.replyTo)
             return
-        ctx.msg("Results for \"help {:s}\": {:s}. Subcommands: {:s}".format(args[0:], self.documentation[0], self.follwingcmds[0]), msg.replyTo)
+        ctx.msg("Usage: !{:s}{:s} About: {:s}. Subcommands: {:}".format(" ".join(args[0:]),
+                                         " " + self.documentation[1] if self.documentation[1] else "",
+                                         self.documentation[0],
+                                         ", ".join(self.followingcmds) if len(self.followingcmds) > 0 else "None."),
+                msg.replyTo)
